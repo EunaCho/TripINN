7028c7eb9bb93ae6524f0a8c729d0c2b4abaafa6
은경 <@192.168.166.1>
2016-12-16 13:14:39 +0900

    d

--- TripINN/src/main/java/com/inn/mypage/MypageDAO.java
+++ TripINN/src/main/java/com/inn/mypage/MypageDAO.java
@@ -1,9 +1,13 @@
 package com.inn.mypage;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Repository;
+
 import com.common.common.AbstractDAO;
 
-@Repository("mypageDAO")
+@Repository("mypageDAO") //DAO 클래스 등록 
 public class MypageDAO extends AbstractDAO {
 	/*
 	@SuppressWarnings
@@ -18,4 +22,18 @@ public class MypageDAO extends AbstractDAO {
 	9. unchecked : 검증되지 않은 연산자 관련 경고 억제
 	10. unused : 사용하지 않는 코드 관련 경고 억제
 	*/
+	
+	//**메시지 
+	//보낸 메시지 리스트
+	@SuppressWarnings("unchecked")//경고 제외 어노테이션
+	public List<Map<String, Object>> selectSendMsgList(Map<String, Object> map){
+		return (List<Map<String, Object>>) selectList("mypage.selectSendMsgList", map);
+	}
+	
+	@SuppressWarnings("unchecked")//경고 제외 어노테이션
+	//받은 메시지 리트스 
+	public List<Map<String, Object>> selectReceiveMsgList(Map<String, Object> map){
+		return (List<Map<String, Object>>) selectList("mypage.selectReceiveMsgList", map);
+	}
+	
 }
--- TripINN/src/main/java/com/inn/mypage/MypageService.java
+++ TripINN/src/main/java/com/inn/mypage/MypageService.java
@@ -6,7 +6,9 @@ import java.util.Map;
 //DAO 기능 구현
 public interface MypageService {
 	
+	//보낸 메시지 리스트 
 	List<Map<String, Object>> selectSendMsgList(Map<String, Object> map) throws Exception;
+	//받은메시지 리스트 
 	List<Map<String, Object>> selectReceiveMsgList(Map<String, Object> map) throws Exception;
 
 }
