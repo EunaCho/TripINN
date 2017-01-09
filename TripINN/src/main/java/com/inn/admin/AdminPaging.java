eb330680371658ddb1c55fbcb5e0a74a0d3dd758
jinwook222 <jinwook222@naver.com>
2017-01-09 14:56:20 +0900

    admin 오류수정

--- TripINN/src/main/java/com/inn/admin/AdminPaging.java
+++ TripINN/src/main/java/com/inn/admin/AdminPaging.java
@@ -2,7 +2,6 @@ package com.inn.admin;
 
 public class AdminPaging {
 
-<<<<<<< HEAD
 	private int currentPage;   // 현재페이지
 	private int totalCount;	 // 전체 게시물 수
 	private int totalPage;	 // 전체 페이지 수
@@ -13,70 +12,43 @@ public class AdminPaging {
 	private int startPage;	 // 시작 페이지
 	private int endPage;	 // 마지막 페이지
 	private String requestName;
-=======
-   private int currentPage;   // 현재페이지
-   private int totalCount;    // 전체 게시물 수
-   private int totalPage;    // 전체 페이지 수
-   private int blockCount;    // 한 페이지의  게시물의 수
-   private int blockPage;    // 한 화면에 보여줄 페이지 수
-   private int startCount;    // 한 페이지에서 보여줄 게시글의 시작 번호
-   private int endCount;    // 한 페이지에서 보여줄 게시글의 끝 번호
-   private int startPage;    // 시작 페이지
-   private int endPage;    // 마지막 페이지
-   private String requestName;
->>>>>>> branch 'master' of https://github.com/EunaCho/TripINN.git
 
-   private StringBuffer pagingHtml;
+	private StringBuffer pagingHtml;
 
-<<<<<<< HEAD
 	// 페이징 생성자(Search없는 생성자)
 	public AdminPaging(int currentPage, int totalCount, int blockCount,
 			int blockPage, String requestName) {
-=======
-   // 페이징 생성자(Search없는 생성자)
-   public AdminPaging(int currentPage, int totalCount, int blockCount,
-         int blockPage, String requestName) {
->>>>>>> branch 'master' of https://github.com/EunaCho/TripINN.git
 
-<<<<<<< HEAD
 		this.blockCount = blockCount;
 		this.blockPage = blockPage;
 		this.currentPage = currentPage;
 		this.totalCount = totalCount;
 		this.requestName = requestName;
-=======
-      this.blockCount = blockCount;
-      this.blockPage = blockPage;
-      this.currentPage = currentPage;
-      this.totalCount = totalCount;
-      this.requestName = requestName;
->>>>>>> branch 'master' of https://github.com/EunaCho/TripINN.git
 
-      // 전체 페이지 수
-      totalPage = (int) Math.ceil((double) totalCount / blockCount);
-      if (totalPage == 0) {
-         totalPage = 1;
-      }
+		// 전체 페이지 수
+		totalPage = (int) Math.ceil((double) totalCount / blockCount);
+		if (totalPage == 0) {
+			totalPage = 1;
+		}
 
-      // 현재 페이지가 전체 페이지 수보다 크면 전체 페이지 수로 설정
-      if (currentPage > totalPage) {
-         currentPage = totalPage;
-      }
+		// 현재 페이지가 전체 페이지 수보다 크면 전체 페이지 수로 설정
+		if (currentPage > totalPage) {
+			currentPage = totalPage;
+		}
 
-      // 현재 페이지의 처음과 마지막 글의 번호 가져오기.
-      startCount = (currentPage - 1) * blockCount;
-      endCount = startCount + blockCount - 1;
+		// 현재 페이지의 처음과 마지막 글의 번호 가져오기.
+		startCount = (currentPage - 1) * blockCount;
+		endCount = startCount + blockCount - 1;
 
-      // 시작 페이지와 마지막 페이지 값 구하기.
-      startPage = (int) ((currentPage - 1) / blockPage) * blockPage + 1;
-      endPage = startPage + blockPage - 1;
+		// 시작 페이지와 마지막 페이지 값 구하기.
+		startPage = (int) ((currentPage - 1) / blockPage) * blockPage + 1;
+		endPage = startPage + blockPage - 1;
 
-      // 마지막 페이지가 전체 페이지 수보다 크면 전체 페이지 수로 설정
-      if (endPage > totalPage) {
-         endPage = totalPage;
-      }
+		// 마지막 페이지가 전체 페이지 수보다 크면 전체 페이지 수로 설정
+		if (endPage > totalPage) {
+			endPage = totalPage;
+		}
 
-<<<<<<< HEAD
 		// 이전 block 페이지
 		pagingHtml = new StringBuffer();
 		if (currentPage > blockPage) {
@@ -200,214 +172,89 @@ public class AdminPaging {
 			}
 				
 		}
-=======
-      // 이전 block 페이지
-      pagingHtml = new StringBuffer();
-      if (currentPage > blockPage) {
-         pagingHtml.append("<a class='page prv' href=" + requestName + ".do?currentPage=" + (startPage - 1) + ">");
-         pagingHtml.append("&lt;");
-         pagingHtml.append("</a>");
-      }
-      
-      pagingHtml.append("&nbsp;|&nbsp;");
-      
-      
-      //페이지 번호, 현재 페이지는 다르게 표시
-      for(int i = startPage;i<=endPage;i++)
-      {
-         if(i > totalPage)
-         {
-            break;
-         }
-         if(i == currentPage)
-         {
-            pagingHtml.append("&nbsp;<b> <font color='red'>");
-            pagingHtml.append(i);
-            pagingHtml.append("</font></b>");
-         }
-         else
-         {
-            pagingHtml.append("&nbsp;<a class='page' href=" + requestName + ".do?currentPage=");
-            pagingHtml.append(i);
-            pagingHtml.append("'>");
-            pagingHtml.append(i);
-            pagingHtml.append("</a>");
-         }
-         
-         pagingHtml.append("&nbsp;");
-      }
-      
-      pagingHtml.append("&nbsp;&nbsp;|&nbsp;&nbsp;");
-      
-      // 다음 block 페이지
-      if(totalPage - startPage >= blockPage)
-      {
-         pagingHtml.append("<a class='page next' href=" + requestName + ".do?currentPage=" + (endPage + 1) + ">");
-         pagingHtml.append((endPage+1));
-         pagingHtml.append("'>");
-         pagingHtml.append("다음");
-         pagingHtml.append("</a>");
-      }
-   }
-   
-   // Search있는 생성자
-      public AdminPaging(int currentPage, int totalCount, int blockCount, int blockPage,String requestName, int searchNum, String isSearch){
-         
-         this.blockCount = blockCount;
-         this.blockPage = blockPage;
-         this.currentPage = currentPage;
-         this.totalCount = totalCount;
-         
-         totalPage = (int) Math.ceil((double) totalCount / blockCount);
-         if(totalPage == 0){
-            totalPage = 1;
-         }
-         
-         if(currentPage > totalPage){
-            currentPage = totalPage;
-         }
-         
-         startCount = (currentPage -1) * blockCount;
-         endCount = startCount + blockCount -1;
-         
-         startPage = (int)((currentPage -1) / blockPage) * blockPage +1;
-         endPage = startPage + blockPage -1;
-         
-         if(endPage > totalPage){
-            endPage = totalPage;
-         }
-         
-         // 이전 block 페이지
-         pagingHtml = new StringBuffer();
-         if(currentPage > blockPage){
-            
-            if(isSearch != "")
-               pagingHtml.append("<a class='page prv' href=" + requestName + ".do?currentPage=" + (startPage - 1) + "&searchKeyword="+isSearch+"&searchNum="+searchNum+">");
-            
-            else                    
-               pagingHtml.append("<a class='page prv' href=" + requestName + ".do?currentPage=" + (startPage - 1) + ">");
-            pagingHtml.append("이전");
-            pagingHtml.append("</a>");
-            }
-         
-         pagingHtml.append("&nbsp;|&nbsp;");
-         
-         //페이지 번호, 현재 페이지는 다르게 표시
-         for(int i = startPage; i <= endPage; i++){
-            if(i > totalPage){
-               break;
-            }
-            if(i == currentPage){
-               pagingHtml.append("<strong>");
-               pagingHtml.append(i);
-               pagingHtml.append("</strong>");
-            }
-            else{
-               pagingHtml.append("&nbsp;<a class='page' href=" + requestName + ".do?currentPage=" + i);
-               if(isSearch != "")
-                  pagingHtml.append("&searchKeyword="+isSearch);
-               pagingHtml.append("'>");
-               pagingHtml.append(i);
-               pagingHtml.append("</a>");
-               
-            }
-            pagingHtml.append("&nbsp;");
-         }
-         // 다음 block 페이지
-         if(totalPage - startPage >= blockPage){
-            pagingHtml.append("<a class='page next' href=" + requestName + ".do?currentPage=" + (endPage + 1));
-            if(isSearch != "")
-               pagingHtml.append("&searchKeyword="+isSearch);
-            pagingHtml.append("'>");
-            pagingHtml.append("다음");
-            pagingHtml.append("</a>");
-         }
-            
-      }
->>>>>>> branch 'master' of https://github.com/EunaCho/TripINN.git
 
-   
+	
 
-   public int getCurrentPage() {
-      return currentPage;
-   }
+	public int getCurrentPage() {
+		return currentPage;
+	}
 
-   public void setCurrentPage(int currentPage) {
-      this.currentPage = currentPage;
-   }
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
 
-   public int getTotalCount() {
-      return totalCount;
-   }
+	public int getTotalCount() {
+		return totalCount;
+	}
 
-   public void setTotalCount(int totalCount) {
-      this.totalCount = totalCount;
-   }
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
 
-   public int getTotalPage() {
-      return totalPage;
-   }
+	public int getTotalPage() {
+		return totalPage;
+	}
 
-   public void setTotalPage(int totalPage) {
-      this.totalPage = totalPage;
-   }
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
 
-   public int getBlockCount() {
-      return blockCount;
-   }
+	public int getBlockCount() {
+		return blockCount;
+	}
 
-   public void setBlockCount(int blockCount) {
-      this.blockCount = blockCount;
-   }
+	public void setBlockCount(int blockCount) {
+		this.blockCount = blockCount;
+	}
 
-   public int getBlockPage() {
-      return blockPage;
-   }
+	public int getBlockPage() {
+		return blockPage;
+	}
 
-   public void setBlockPage(int blockPage) {
-      this.blockPage = blockPage;
-   }
+	public void setBlockPage(int blockPage) {
+		this.blockPage = blockPage;
+	}
 
-   public int getStartCount() {
-      return startCount;
-   }
+	public int getStartCount() {
+		return startCount;
+	}
 
-   public void setStartCount(int startCount) {
-      this.startCount = startCount;
-   }
+	public void setStartCount(int startCount) {
+		this.startCount = startCount;
+	}
 
-   public int getEndCount() {
-      return endCount;
-   }
+	public int getEndCount() {
+		return endCount;
+	}
 
-   public void setEndCount(int endCount) {
-      this.endCount = endCount;
-   }
+	public void setEndCount(int endCount) {
+		this.endCount = endCount;
+	}
 
-   public int getStartPage() {
-      return startPage;
-   }
+	public int getStartPage() {
+		return startPage;
+	}
 
-   public void setStartPage(int startPage) {
-      this.startPage = startPage;
-   }
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
 
-   public int getEndPage() {
-      return endPage;
-   }
+	public int getEndPage() {
+		return endPage;
+	}
 
-   public void setEndPage(int endPage) {
-      this.endPage = endPage;
-   }
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
 
-   public StringBuffer getPagingHtml() {
-      return pagingHtml;
-   }
+	public StringBuffer getPagingHtml() {
+		return pagingHtml;
+	}
 
-   public void setPagingHtml(StringBuffer pagingHtml) {
-      this.pagingHtml = pagingHtml;
-   }
-   
-   
+	public void setPagingHtml(StringBuffer pagingHtml) {
+		this.pagingHtml = pagingHtml;
+	}
+	
+	
 
 }
